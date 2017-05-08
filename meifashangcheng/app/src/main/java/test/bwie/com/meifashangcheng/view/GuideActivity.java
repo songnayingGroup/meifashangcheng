08209aa50fa7fd45718253c95c3b900578f4a025
OOMBest <273092218@qq.com>
2017-05-08 20:37:32 +0800

    ver:001

---
+++ meifashangcheng/app/src/main/java/test/bwie/com/meifashangcheng/view/GuideActivity.java
@@ -0,0 +1,18 @@
+package test.bwie.com.meifashangcheng.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import test.bwie.com.meifashangcheng.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+
+
+    }
+}
